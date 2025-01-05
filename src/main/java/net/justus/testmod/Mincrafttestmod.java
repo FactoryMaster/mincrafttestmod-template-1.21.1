6b48e88ac36031c4bf5cd87c5ea1fec138187c24
Justus <factorymasterlive@gmail.com>
2025-01-05 21:58:16 +0100

    test

---
+++ src/main/java/net/justus/testmod/Mincrafttestmod.java
@@ -0,0 +1,18 @@
+package net.justus.testmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+//test
+public class Mincrafttestmod implements ModInitializer {
+	public static final String MOD_ID = "mincrafttestmod";
+
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/justus/testmod/MinecrafttestmodClient.java
@@ -0,0 +1,10 @@
+package net.justus.testmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinecrafttestmodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
