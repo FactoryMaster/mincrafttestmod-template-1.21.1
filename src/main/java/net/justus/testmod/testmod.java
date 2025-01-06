ff378218e9da704208c9e1665d3739a6c2a7c525
Justus <factorymasterlive@gmail.com>
2025-01-06 18:03:44 +0100

    Neue Item gruppe

--- src/main/java/net/justus/testmod/testmod.java
+++ src/main/java/net/justus/testmod/testmod.java
@@ -3,6 +3,7 @@ package net.justus.testmod;
 import net.fabricmc.api.ModInitializer;
 
 import net.justus.testmod.block.ModBlocks;
+import net.justus.testmod.item.ModItemGroups;
 import net.justus.testmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +16,10 @@ public class testmod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+
+		ModItemGroups.registerItemGroups();
+
+
 		ModItems.registerModItems();
 		ModBlocks.registerModBlcks();
 	}
